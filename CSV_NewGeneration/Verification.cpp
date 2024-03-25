e76be606e4565c5ca00f2373a44824669e7f6312
Benjamin <>
2024-03-25 10:57:59 +0100

    extra fix

--- CSV_NewGeneration/Verification.cpp
+++ CSV_NewGeneration/Verification.cpp
@@ -49,6 +49,8 @@ static void proccess_file(const fs::path& current_path, const std::function<void
 
 		on_line(current_line);
 	}
+
+	current_file.close();
 }
 
 int main() {
@@ -57,41 +59,47 @@ int main() {
 	std::forward_list<fs::path> file_paths;
 	get_files(data_filepath, file_paths);
 
-	for (const fs::path file : file_paths) {
-		double user_acceleration_x = 0;
-		double user_acceleration_y = 0;
-		double user_acceleration_z = 0;
-		uint64_t total_lines = 0;
+	double user_acceleration_x = 0;
+	double user_acceleration_y = 0;
+	double user_acceleration_z = 0;
+	uint64_t total_lines = 0;
 
+	for (const fs::path file : file_paths) {
 		proccess_file(file, [&](const Line& line) {
 			user_acceleration_x += line.user_acceleration_x;
 			user_acceleration_y += line.user_acceleration_y;
 			user_acceleration_z += line.user_acceleration_z;
 			total_lines++;
 		});
-	
-		double average_acceleration_x = user_acceleration_x / total_lines;
-		double average_acceleration_y = user_acceleration_y / total_lines;
-		double average_acceleration_z = user_acceleration_z / total_lines;
-		
-		double squared_deviations_x = 0;
-		double squared_deviations_y = 0;
-		double squared_deviations_z = 0;
+	}
 
+	double average_acceleration_x = user_acceleration_x / total_lines;
+	double average_acceleration_y = user_acceleration_y / total_lines;
+	double average_acceleration_z = user_acceleration_z / total_lines;
+
+	double squared_deviations_x = 0;
+	double squared_deviations_y = 0;
+	double squared_deviations_z = 0;
+
+	for (const fs::path file : file_paths) {
 		proccess_file(file, [&](const Line& line) {
 			squared_deviations_x += std::pow(line.user_acceleration_x - average_acceleration_x, 2);
 			squared_deviations_y += std::pow(line.user_acceleration_y - average_acceleration_y, 2);
 			squared_deviations_z += std::pow(line.user_acceleration_z - average_acceleration_z, 2);
 		});
-
-		double std_deviation_x = std::sqrt(squared_deviations_x / total_lines);
-		double std_deviation_y = std::sqrt(squared_deviations_y / total_lines);
-		double std_deviation_z = std::sqrt(squared_deviations_z / total_lines);
-	
-		std::cout << "Standard Deviation X: " << std_deviation_x << std::endl;
-		std::cout << "Standard Deviation Y: " << std_deviation_y << std::endl;
-		std::cout << "Standard Deviation Z: " << std_deviation_z << std::endl;
 	}
+
+	double std_deviation_x = std::sqrt(squared_deviations_x / total_lines);
+	double std_deviation_y = std::sqrt(squared_deviations_y / total_lines);
+	double std_deviation_z = std::sqrt(squared_deviations_z / total_lines);
+
+	std::cout << "Average X: " << average_acceleration_x << std::endl;
+	std::cout << "Average Y: " << average_acceleration_y << std::endl;
+	std::cout << "Average Z: " << average_acceleration_z << std::endl;
+
+	std::cout << "Standard Deviation X: " << std_deviation_x << std::endl;
+	std::cout << "Standard Deviation Y: " << std_deviation_y << std::endl;
+	std::cout << "Standard Deviation Z: " << std_deviation_z << std::endl;
 
 	return EXIT_SUCCESS;
 }
