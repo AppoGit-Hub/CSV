dd75d149fc6247d621dcbd5fa7a53ecf0fae108c
Benjamin <>
2024-05-24 00:51:45 +0200

    removed unsused files

--- CSV_NewGeneration/Pattern_xyz.cpp
+++
@@ -1,9 +0,0 @@
-#include "Global.hpp"
-
-void create_header_xyz_pattern(std::fstream& file) {
-	file << "Mouvement";
-	for (size_t index = 0; index < TRAINSET_COLUMNS; index++) {
-		file << DELIMITER << "Vacc";
-	}
-	file << std::endl;
-}
